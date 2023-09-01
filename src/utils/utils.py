650c165f8413dedebd96a386d3d7bdf8dfe05558
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-09-01 12:06:02 +0530

    Update src and tests

---
+++ src/utils/utils.py
@@ -0,0 +1,8 @@
+"""Ultilities"""
+
+def get_none():
+    """
+    This function return None
+    Its purpose is for pytest GitHub Actions demo
+    """
+    return None
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,10 @@
 
 from src.utils.utils import get_none
 
+
 def test_get_none():
     """
     This function return None
     Its purpose is for pytest GitHub Actions demo
     """
-    assert get_none() == None
+    assert get_none() is None
