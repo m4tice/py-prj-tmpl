a47ee7cbde62d8a207a28d7784cfbf4d9c71b055
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-09-01 10:41:40 +0530

    [ADD]-pylint template examples

---
+++ template_with_pylint_warning.py
@@ -0,0 +1,14 @@
+# pylint: skip-file  <-- delete this line to see the warnings
+
+
+# Possible pylint warning:
+# - 1. Missing module docstring Pylint(C0114:missing-module-docstring)
+# - 2. Final newline missing Pylint(C0304:missing-final-newline)
+# - 3. Missing function or method docstring Pylint(C0116:missing-function-docstring)
+# - 4. Line too long (129/100) Pylint(C0301:line-too-long)
+
+def print_hello_world():
+
+
+
+    print("Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd ")
---
+++ template_without_pylint_warning.py.py
@@ -0,0 +1,16 @@
+# pylint: skip-file  <-- delete this line to see the warnings
+"""This is a module docstring (1)"""
+
+# Possible pylint warning:
+# - 1. Missing module docstring Pylint(C0114:missing-module-docstring)
+# - 2. Final newline missing Pylint(C0304:missing-final-newline)
+# - 3. Missing function or method docstring Pylint(C0116:missing-function-docstring)
+# - 4. Line too long (129/100) Pylint(C0301:line-too-long)
+
+def print_hello_world():
+    """
+    This is a function / method docstring
+    """
+    print("Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd Hello Wolrd ")  #pylint: disable=line-too-long
+
+# This is a new empty line but it resolves newline missing warning
