81eb243f61e4571566ecb725dc09d4e3360996a3
m4tice <ductuan2908@gmail.com>
2024-10-05 10:19:05 +0700

    create: init_structure.bat

---
+++ init_structure.bat
@@ -0,0 +1,10 @@
+mkdir app
+mkdir app\templates
+mkdir app\static
+
+echo.> app/__init__.py
+echo.> app/routes.py
+echo.> app/models.py
+echo.> app/templates/index.html
+echo.> app/static/style.css
+@REM echo.> run.py
